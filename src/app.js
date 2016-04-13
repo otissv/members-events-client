6918f8a9e2fa831810138ac59615b107b5acd7d7
otissv <otissv@gmail.com>
2016-04-13 14:17:02 +0200

    added redux-dev-tools

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@
  */
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
+import DevTools from './containers/dev-tools-container.js';
 import routes from './routes.js';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
+)(createStore)(reducers);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
   </Provider>,
   document.getElementById('mount-node')
