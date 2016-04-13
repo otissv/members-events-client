93b48ddc139fe67d5fde0a3efaf99f4e6f894794
otissv <otissv@gmail.com>
2016-04-13 17:06:30 +0200

    extracted component from container

--- src/components/application/layout-application.jsx
+++ src/components/application/layout-application.jsx
@@ -3,18 +3,12 @@
  */
 
 'use strict';
-import { connect } from 'react-redux';
 import Navigation from './navigation-application.jsx';
 import Alert from './alert-appplication.jsx';
 import Notify from './notify-appplication.jsx';
-import actions from '../../actions';
 
 
-class Layout extends React.Component  {
-  componentWillMount () {
-    // this.props.getStorge(['_id', 'token']);
-  }
-
+export default class Layout extends React.Component  {
   render () {
     return <div>
       <Alert/>
@@ -28,19 +22,5 @@ class Layout extends React.Component  {
 
 Layout.propTypes = {
   isLoggedIn: React.PropTypes.bool,
-  children: React.PropTypes.any,
-  getStorge: React.PropTypes.func
+  children: React.PropTypes.any
 };
-
-
-function mapStateToProps (state) {
-  return {
-    storage : state.storage,
-    isLoggedIn: state.auth.isLoggedIn
-  };
-}
-
-
-export default connect(mapStateToProps, {
-  getStorge : actions.getStorge
-})(Layout);
---
+++ src/containers/layout-container.jsx
@@ -0,0 +1,33 @@
+/*
+* Application layout
+ */
+
+'use strict';
+import { connect } from 'react-redux';
+import actions from '../../actions';
+import Layout from '../components/application/layout-application.jsx';
+
+
+export default function(ComposedClass, redirect) {
+  class LayoutContainer extends React.Component  {
+    componentWillMount () {
+      // this.props.getStorge(['_id', 'token']);
+    }
+
+    render () {
+      return <Layout />;
+    }
+  }
+
+  function mapStateToProps (state) {
+    return {
+      storage : state.storage,
+      isLoggedIn: state.auth.isLoggedIn
+    };
+  }
+
+
+  return connect(mapStateToProps, {
+    getStorge : actions.getStorge
+  })(LayoutContainer);
+}
